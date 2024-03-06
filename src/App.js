b31b1eeea90dad948795094a522bd713634647dc
hajarbnh33 <hb-79@hotmail.fr>
2024-03-06 15:53:03 +0100

    projet 6

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const name = "Hajar !";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bienvenue{name}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
